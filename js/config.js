ab2a34bfbffdf921192d6fa7d5d38163aacdc0c1
Ari Bulas Cruz <36916486+abulascruz@users.noreply.github.com>
2026-03-05 20:56:03 -0300

    Create js folder and config.js

---
+++ js/config.js
@@ -0,0 +1,14 @@
+const DATASETS = {
+  books: {
+    name: "Obras",
+    file: "datasets/books.csv"
+  },
+  movies: {
+    name: "Autorias",
+    file: "datasets/autorias.csv"
+  },
+  games: {
+    name: "Eventos",
+    file: "datasets/eventos.csv"
+  }
+};
